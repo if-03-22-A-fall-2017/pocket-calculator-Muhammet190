c345c1a5b2af01c8a36066058381cba2b69c8951
Halil Özdogan <superman.batboy@outlook.com>
2017-10-01 14:50:24 +0200

    File added

--- pocketcalculator.c
+++ pocketcalculator.c
@@ -1,46 +1,71 @@
 #include <stdio.h>
+
 int menu(int);
-double add(double,double);
-double subtract(double,double);
-double multiply(double,double);
-double divide(double,double);
+void getoperands(double*,double*);
+double performoperation(int,double,double);
+
 
 int main(int argc, char const *argv[])
 {
-double operand1 = 0;
-double operand2 = 0;
-
 int choose = 0;
 menu(choose);
-printf("Geben Sie nun die 1.Operande ein\n");
-scanf("%f",&operand1);
-printf("Geben Sie nun die 2.Operande ein\n");
-scanf("%f",&operand2);
+
+double operand1 = 0;
+double operand2 = 0;
+getoperands(&operand1,&operand2);
+performoperation(choose,operand1,operand2);
+}
+
+  int menu(int choose)
+  {
+    do {
+      printf("Choose one of the following operations:\n");
+      printf("Add (1)\n");
+      printf("Subtract (2)\n");
+      printf("Multiply (3)\n");
+      printf("Divide (4)\n");
+      printf("Stop program (-1)\n");
+      printf("Enter your choice:\n");
+      scanf("%d", &choose);
+
+    } while(choose >= 5 || choose <= -2);
+
+    return choose;
+  }
+
+    void getoperands(double *operand1,double *operand2)
+    {
+      printf("Geben Sie nun die 1.Operande ein\n");
+      scanf("%lf",operand1);
+      printf("Geben Sie nun die 2.Operande ein\n");
+      scanf("%lf",operand2);
+    }
+double performoperation(int choose,double operand1,double operand2){
+double result = 0;
 switch (choose)
 {
-  case /* value */:
+  case 1:
+  result = operand1 + operand2;
+  break;
+
+  case 2:
+  result = operand1 - operand2;
+  break;
+
+  case 3:
+  result = operand1 * operand2;
+  break;
+
+  case 4:
+  if(operand1 == 0 || operand2 == 0){
+    printf("Division by zero.\n");
+  }else{
+    result = operand1 / operand2;
+  }
+  break;
+
+  default:
+  break;
 }
-
-}
-
-int menu(int choose)
-{
-  do {
-    printf("Choose one of the following operations:\n");
-    printf("Add (1)\n");
-    printf("Subtract (2)\n");
-    printf("Multiply (3)\n");
-    printf("Divide (4)\n");
-    printf("Stop program (-1)\n");
-    printf("Enter your choice:\n");
-    scanf("%d", &choose);
-
-  } while(choose >= 5 || choose <= -2);
-
-  return choose;
-}
-double add(double operand1)
-{
-  return -operand1;
-
+return result;
 }
